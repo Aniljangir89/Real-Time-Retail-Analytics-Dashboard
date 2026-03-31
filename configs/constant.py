55fca226d09378445f2c5ee7b2ce3661c5e28d6d
Aniljangir89 <aniljangid46285@gmail.com>
2026-03-31 11:19:20 +0000

    completed upto bronze layer all the tasks

--- configs/constant.py
+++ configs/constant.py
@@ -3,3 +3,7 @@ CONNECTION_STRING = "Endpoint=sb://eventhub-de-training-2026.servicebus.windows.
 NAMESPACE = "eventhub-de-training-2026"
 
 
+# storage account connection 
+
+STORAGE_ACCOUNT_NAME = "adlsgen2detraining2026"
+STORAGE_ACCOUNT_ACCESS_KEY = "HSpKvFTs8PcW35K6Uf4LwCw1XY4VY6/95uu4QROItw2vheNHCfFA8w/HN1Qvja0TEuB4DS56Hspz+AStLv3mTw=="
