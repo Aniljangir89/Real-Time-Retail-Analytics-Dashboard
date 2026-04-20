e66e2464a7dd7dc3f3c543fb9280ba6252839d66
AS-MAC-1336 <as-mac-1336@AS-MAC-1336s-MacBook-Air.local>
2026-04-20 12:44:12 +0530

    updated producer

--- source-app/retail_app.py
+++ source-app/retail_app.py
@@ -5,11 +5,25 @@ from datetime import datetime
 fake = Faker()
 
 def generate_event():
-    return {
+
+    event_type = "search"
+    if random.random() < 0.6:   # 60% click after search
+        event_type = "click"
+
+        if random.random() < 0.5:  # 50% go to cart
+            event_type = "cart"
+
+            if random.random() < 0.4:  # 40% checkout
+                event_type = "checkout"
+
+                if random.random() < 0.5:  # 50% purchase
+                    event_type = "purchase"
+
+    event = {
         "event_id": fake.uuid4(),
         "user_id": fake.random_int(1, 1000),
         "session_id": fake.uuid4(),
-        "event_type": random.choice(["search", "click", "cart", "checkout", "purchase"]),
+        "event_type": event_type,
         "product_id": fake.random_int(100, 200),
         "category": random.choice(["electronics", "fashion"]),
         "timestamp": datetime.utcnow().isoformat(),
@@ -17,3 +31,18 @@ def generate_event():
         "device_type": random.choice(["mobile", "web"]),
         "app_version": "1.0.0"
     }
+
+    if event_type == "click":
+        event["price"] = random.randint(100, 1000)
+
+    elif event_type == "cart":
+        event["quantity"] = random.randint(1, 5)
+
+    elif event_type == "checkout":
+        event["cart_total"] = random.randint(500, 5000)
+
+    elif event_type == "purchase":
+        event["amount"] = random.randint(500, 5000)
+        event["payment_method"] = random.choice(["card", "upi"])
+
+    return event
